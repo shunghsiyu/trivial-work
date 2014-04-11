474692b94e2498e2ed3ba7c1496be2964e4a7abf
shunghsiyu <shunghsiyu@gmail.com>
2014-04-11 16:06:29 -0400

    Updated CashRegister.java
    Added java-doc style comment to the CashRegister class.

--- src/programming2/ch8/cashregister/CashRegister.java
+++ src/programming2/ch8/cashregister/CashRegister.java
@@ -28,24 +28,48 @@ public class CashRegister {
     public static final double TAX_RATE = 0.06; 
     private final RetailItem retailItem;
     private final int quantity;
-
+    
+    /**
+     * Constructor for CashRegister class.
+     * @param retailItem specified retailItem for this CashRegister instance
+     * @param quantity quantity of the item
+     */
     public CashRegister(RetailItem retailItem, int quantity) {
         this.retailItem = new RetailItem(retailItem);
         this.quantity = quantity;
     }
     
+    /**
+     * Get the subtotal of this CashRegister instance. Subtotal is the price
+     * multiplied by the quantity of the item.
+     * @return subtotal of this CashRegister instance
+     */
     public double getSubTotal() {
         return retailItem.getPrice() * quantity;
     }
     
+    /**
+     * Get the tax that would be applied to the subtotal.
+     * @return amount tax that will be applied
+     */
     public double getTax() {
         return getSubTotal() * TAX_RATE;
     }
     
+    /**
+     * Get the total cost. Total cost is the subtotal plus tax.
+     * @return the total cost of this CashRegister instance
+     */
     public double getTotal() {
         return getSubTotal() + getTax();
     }
     
+    /**
+     * Get the receipt in tax for this CashRegister instance. The receipt 
+     * includes the description, unit price, quantity of the item, and the 
+     * subtotal, tax and total.
+     * @reutrn receipt of this CashRegister instance.
+     */
     public String getReceipt() {
         int columnSpace = 12;
         StringBuilder sb = new StringBuilder();
