e7946e7b196506a6fe3326ef80174f15546e95e4
Shung-Hsi Yu <shunghsiyu@gmail.com>
2014-04-11 22:24:13 -0400

    Updated CashRegisterTester.java Added comment in the main function

--- src/programming2/ch8/cashregister/CashRegisterTester.java
+++ src/programming2/ch8/cashregister/CashRegisterTester.java
@@ -13,19 +13,26 @@ import java.io.PrintWriter;
 
 public class CashRegisterTester {
     public static void main(String[] args) {
+        // Create a new RetailItem instance of jacket
         RetailItem retailItem = new RetailItem("Jacket", 8, 59.95);
+        // Create a new CashRegister instance for two jackets
         CashRegister cashRegister = new CashRegister(retailItem, 2);
+        // Get the receipt from the CashRegister instance
         String receipt = cashRegister.getReceipt();
         
+        // Prepare file to write to
         File receiptFile = new File("receipt.txt");
         try (PrintWriter output = new PrintWriter(receiptFile)) {
+            // Write the receipt to file
             output.append(receipt);
         } catch (FileNotFoundException ex) {
+            // Print error
             System.out.println("Can't write to file " + 
                     receiptFile.getAbsolutePath());
             System.out.println("File not found");
         }
         
+        // Also print the receipt on the screen
         System.out.println(receipt);
     }
 }
