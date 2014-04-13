0adf2c0fa8594fb5958673afad5106ab44075eea
Shung-Hsi Yu <shunghsiyu@gmail.com>
2014-04-13 01:17:05 -0400

    Updated RoomCarpet.java Added java-doc style comment to RoomCarpet class

--- src/programming2/ch8/carpetcalculator/RoomCarpet.java
+++ src/programming2/ch8/carpetcalculator/RoomCarpet.java
@@ -24,15 +24,28 @@ public class RoomCarpet {
     private final RoomDimension size;
     private final double carpetCost;
 
+    /**
+     * Constructor for the RoomCarpet class.
+     * @param dim dimension of the room
+     * @param cost cost of the carpet per square feet
+     */
     public RoomCarpet(RoomDimension dim, double cost) {
         this.size = dim;
         this.carpetCost = cost;
     }
     
+    /**
+     * Get the total cost of this RoomCarpet instance.
+     * @return the total cost
+     */
     public double getTotalCost() {
         return size.getArea()*carpetCost;
     }
     
+    /**
+     * Convert this RoomCarpet instance to a String representation.
+     * @return String representation of this RoomCarpet instance
+     */
     @Override
     public String toString() {
         return size.toString() + " * $" + carpetCost + "/sqft = $" + 
