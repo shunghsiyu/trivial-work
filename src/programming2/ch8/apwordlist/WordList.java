9e41b8327f83db5956e44324cf5e4f421aef1335
Shung-Hsi Yu <shunghsiyu@gmail.com>
2014-04-14 10:38:10 -0400

    Updated WordList.java. Added comments and moved author name to top.

--- src/programming2/ch8/apwordlist/WordList.java
+++ src/programming2/ch8/apwordlist/WordList.java
@@ -1,7 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * AP Word List.
+ * @author Shung-Hsi Yu <syu07@nyit.edu> ID#0906172
+ * @version Apr 9, 2014
  */
 
 package programming2.ch8.apwordlist;
@@ -9,26 +9,38 @@ package programming2.ch8.apwordlist;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-/**
- *
- * @author Shung-Hsi Yu <syu07@nyit.edu> ID#0906172
- * @version Apr 9, 2014
- */
+
 public class WordList {
     private final ArrayList myList;
 
+    /**
+     * Constructor of WordList class. Copies the input ArrayList.
+     * @param myList array list of words as String
+     */
     public WordList(ArrayList<String> myList) {
         this.myList = new ArrayList<>(myList);
     }
-    
+
+    /**
+     * Constructor of WordList class. Copies the input array.
+     * @param stringArray array of words as String
+     */
     public WordList(String[] stringArray) {
         this.myList = new ArrayList<>(Arrays.asList(stringArray));
     }
     
+    /**
+     * Check how many word in this WordList instance has the specified length.
+     * @param len specified word length to count
+     * @return the number of words with the specified length
+     */
     public int numWordsOfLength(int len) {
         int num = 0;
         for(Object o: myList) {
+            // To be safe, checks if the element in the array list are
+            // instance of String
             if(o instanceof String) {
+                // Cast the element of the ArrayList to String
                 String s = (String) o;
                 if(len == s.length()) {
                     num = num + 1;
@@ -41,9 +53,17 @@ public class WordList {
         return num;
     }
     
+    /**
+     * Remove the word with the specified length from this WordList instance.
+     * @param len specified word length to remove
+     */
     public void removeWordsOfLength(int len) {
+        // Start from the end of ArrayList so remove(int i) method will not 
+        // affect the for loop
         for(int i = myList.size()-1; i >= 0; i--) {
             Object o = myList.get(i);
+            // To be safe, checks if the element in the array list are
+            // instance of String
             if(o instanceof String) {
                 String s = (String) o;
                 if(len == s.length()) {
@@ -56,6 +76,10 @@ public class WordList {
         }
     }
     
+    /**
+     * Get the String representation of this WordList instance.
+     * @return String representation of this WordList instance
+     */
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
