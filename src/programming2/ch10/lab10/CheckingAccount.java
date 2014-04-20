155c4b40bde794d52d99bd24fcb278d3aadeabf1
shunghsiyu <shunghsiyu@gmail.com>
2014-04-19 23:58:20 -0400

    Update CheckingAccount.java
    Added javadoc style comment.

--- src/programming2/ch10/lab10/CheckingAccount.java
+++ src/programming2/ch10/lab10/CheckingAccount.java
@@ -13,13 +13,23 @@ package programming2.ch10.lab10;
  */
 public class CheckingAccount extends BankAccount {
     public static final double FEE = 0.15;
-
+    
+    /**
+     * Constructor for the CheckingAccount class.
+     * @param name the name of the checking account owner
+     * @param amount the initial amount this checking account starts with
+     */
     public CheckingAccount(String name, double amount) {
         super(name, amount);
         String oldAccountNumber = super.getAccountNumber();
         super.setAccountNumber("10" + oldAccountNumber);
     }
-
+    
+    /**
+     * Withdraw the specified fee from this checking account. 
+     * A fee will be applied.
+     * @param amount the amount of money to withdraw
+     */
     @Override
     public boolean withdraw(double amount) {
         return super.withdraw(amount + FEE);
