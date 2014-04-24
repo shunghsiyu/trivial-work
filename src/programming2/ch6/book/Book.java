e30477bb1e7319be4511b397442cccbdb34ff0d7
shunghsiyu <shunghsiyu@gmail.com>
2014-04-23 23:58:53 -0400

    Update Book.java
    Added javadoc-style comment

--- src/programming2/ch6/book/Book.java
+++ src/programming2/ch6/book/Book.java
@@ -16,6 +16,13 @@ public class Book {
     private String publisher;
     private int copiesSold;
 
+    /**
+     * Constructor for Book class
+     * @param title title of the book
+     * @param author author of the book
+     * @param publisher publisher of the book
+     * @param copiesSold number of copies this this title has sold
+     */
     public Book(String title, String author, String publisher, int copiesSold) {
         this.title = title;
         this.author = author;
@@ -23,22 +30,42 @@ public class Book {
         this.copiesSold = copiesSold;
     }
 
+    /**
+     * Get the title of the book
+     * @return title of the book
+     */
     public String getTitle() {
         return title;
     }
-
+    
+    /**
+     * Set the title of the book
+     * @param title title of the book
+     */
     public void setTitle(String title) {
         this.title = title;
     }
 
+    /**
+     * Get the author of the book
+     * @return author of the book
+     */
     public String getAuthor() {
         return author;
     }
 
+    /**
+     * Set the author of the book
+     * @param author
+     */
     public void setAuthor(String author) {
         this.author = author;
     }
 
+    /**
+     * Get the publisher of the book
+     * @return publisher of the book
+     */
     public String getPublisher() {
         return publisher;
     }
