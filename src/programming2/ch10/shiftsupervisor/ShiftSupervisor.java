6764111617b73ded672ca7c8b79c3d3b58c930e3
Shung-Hsi Yu <shunghsiyu@gmail.com>
2014-04-27 09:15:59 -0400

    Updated ShiftSupervisor.java Added UML diagram and javadoc style comment.

--- src/programming2/ch10/shiftsupervisor/ShiftSupervisor.java
+++ src/programming2/ch10/shiftsupervisor/ShiftSupervisor.java
@@ -1,7 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/*                                 UML
+ ----------------------------------------------------------------------------
+ |                          ShiftSupervisor                                 |
+ ----------------------------------------------------------------------------
+ | - salary: double                                                         |
+ | - bonus: double                                                          |
+ ----------------------------------------------------------------------------
+ | + ShiftSupervisor()                                                      |
+ | + ShiftSupervisor(name: String, employeeNumber:String,                   |
+ |     hireDateString: String, annualSalary: double, productionBonus: double)
+ | + getSalary(): double                                                    |
+ | + setSalary(salary: double): void                                        |
+ | + getBonus(): double                                                     |
+ | + setBonus(bonus: double): void                                          |
+ | + toString(): String                                                     |
+ ----------------------------------------------------------------------------
  */
 
 package programming2.ch10.shiftsupervisor;
@@ -17,14 +29,27 @@ import programming2.ch10.employee.Employee;
  */
 
 public class ShiftSupervisor extends Employee {
+    // For formatting money
     private static final DecimalFormat decimalFormat = 
             new DecimalFormat("###,###,###,###.00");
     private double salary;
     private double bonus;
 
+    /**
+     * Default constructor for ShiftSupervisor class.
+     */
     public ShiftSupervisor() {
     }
 
+    /**
+     * Constructor for ShiftSupervisor class.
+     * @param name name of the shift supervisor
+     * @param employeeNumber shift supervisor's number
+     * @param hireDateString date the shift supervisor was hired
+     * @param annualSalary the shift supervisor's annual salary
+     * @param productionBonus the shift supervisor's bonus when the shift meets 
+     * the requirement
+     */
     public ShiftSupervisor(String name, String employeeNumber, 
             String hireDateString, double annualSalary, double productionBonus) 
     {
@@ -33,22 +58,42 @@ public class ShiftSupervisor extends Employee {
         this.bonus = productionBonus;
     }
 
+    /** 
+     * Get the salary of this shift supervisor.
+     * @return the employee's annual salary
+     */
     public double getSalary() {
         return salary;
     }
 
+    /**
+     * Set the salary of this shift supervisor.
+     * @param salary the employee's annual salary
+     */
     public void setSalary(double salary) {
         this.salary = salary;
     }
 
+    /**
+     * Get the shift supervisor's bonus when the shift meets the requirement.
+     * @return shift supervisor's bonus
+     */
     public double getBonus() {
         return bonus;
     }
 
+    /**
+     * Set the shift supervisor's bonus when the shift meets the requirement.
+     * @param bonus shift supervisor's bonus
+     */
     public void setBonus(double bonus) {
         this.bonus = bonus;
     }
     
+    /**
+     * Return a string representation of this ShiftSupervisor instance.
+     * @return string representation
+     */
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
