4d04654e5adbaea555fc91d45a7c71b463a023f0
shunghsiyu <shunghsiyu@gmail.com>
2014-09-27 18:03:49 -0400

    Create project assignment1
    assignment1 includes stubs for essential classes Tester, LinkedList and
    BinarySearchTree.

---
+++ assignment1/src/assignment1/BinarySearchTree.java
@@ -0,0 +1,17 @@
+package assignment1;
+
+public class BinarySearchTree {
+
+	public BinarySearchTree() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void insert() {
+		// TODO implement
+	}
+	
+	public boolean find(int value) {
+		// TODO implement
+		return false;
+	}
+}
---
+++ assignment1/src/assignment1/LinkedList.java
@@ -0,0 +1,17 @@
+package assignment1;
+
+public class LinkedList {
+
+	public LinkedList() {
+		// TODO implement
+	}
+
+	public void insert() {
+		// TODO implement
+	}
+	
+	public boolean find(int value) {
+		// TODO implement
+		return false;
+	}
+}
