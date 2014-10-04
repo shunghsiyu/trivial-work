6a92f3c3c61e7ec0b35d14dffdc3cd825eda61b2
Dandan <dandan.wang1129@gmail.com>
2014-10-04 10:09:33 -0400

    LinkedList

--- assignment1/src/assignment1/LinkedList.java
+++ assignment1/src/assignment1/LinkedList.java
@@ -1,17 +1,49 @@
 package assignment1;
 
+class ListNode{
+	int val;
+	ListNode next;
+	public ListNode(int x){
+		val=x;
+		next=null;
+	}
+}
 public class LinkedList {
-
-	public LinkedList() {
-		// TODO implement
-	}
-
-	public void insert(int value) {
-		// TODO implement
-	}
+	ListNode dummy=new ListNode(0);
+	ListNode node=dummy;
 	
-	public boolean find(int value) {
-		// TODO implement
+	public void insert(int val){
+		//insert from the end,too slow
+	/*	node=dummy;
+		while(node.next!=null){
+			if(node.next.val==val)
+				;
+			node=node.next;
+		}
+		node.next=new ListNode(val);*/
+		
+		
+		node=dummy;
+		while(node.next!=null){
+			if(find(val))
+				;
+			ListNode newNode=new ListNode(val);
+			newNode.next=node.next;
+			node.next=newNode;
+			
+		}
+		node=new ListNode(val);
+		}
+	
+	
+	public boolean find(int val){
+		node=dummy;
+		while(node.next!=null){
+			if(node.next.val==val){
+				return true;
+         			}
+			node=node.next;
+		}
 		return false;
 	}
 }
