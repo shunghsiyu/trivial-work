934c45dd5674d9d4343713392569b4b9d73e88dc
diamondlyj <Roger20001107@gmail.com>
2014-10-07 23:01:04 -0400

    Update BinarySearchTree.java
    paste my code

--- assignment1/src/assignment1/BinarySearchTree.java
+++ assignment1/src/assignment1/BinarySearchTree.java
@@ -1,17 +1,61 @@
 package assignment1;
 
 public class BinarySearchTree {
+  private Node root;
 
-	public BinarySearchTree() {
-		// TODO Auto-generated constructor stub
-	}
+   public BinarySearchTree() { 
+   this.root=null;
+   }  
+   public void insert(int value){
+    root=insert(root,value);
+ }
+   
+public class Node
+   {
+      public int value;
+      public Node right;
+      public Node left;
+      
+  public Node(int value){
+  this.value=value;
+  this.right=null;
+  this.left=null;
+  }
+      }
 
-	public void insert(int value) {
-		// TODO implement
-	}
-	
-	public boolean find(int value) {
-		// TODO implement
-		return false;
-	}
+ public Node insert(Node treeNode,int value){
+if(treeNode==null){
+    treeNode=new Node(value);
+}
+else{
+    if(value>=treeNode.value){
+        treeNode.right=insert(treeNode.right,value);
+        }
+    else{
+        treeNode.left=insert(treeNode.left,value);
+    }
+}
+return treeNode;
+ }
+ 
+     
+ 
+ 
+public boolean find(int value){
+   Node findTreeNode=root;
+   if(findTreeNode.value==value){
+       return true;
+ }
+ else{
+       if(value>findTreeNode.value){
+           findTreeNode=findTreeNode.right;
+           return true;
+       }
+       else{
+           findTreeNode=findTreeNode.left;
+           return true;
+       }
+   } 
+ }
+
 }
