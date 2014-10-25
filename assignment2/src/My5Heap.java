e4eaf96c719d08bbe553a1fe00cdfa2ded40b3c2
shunghsiyu <shunghsiyu@gmail.com>
2014-10-25 13:59:22 -0400

    Initial upload for Assignment 2

---
+++ assignment2/src/My5Heap.java
@@ -0,0 +1,15 @@
+
+public class My5Heap {
+
+	public My5Heap() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void insert(int priority, String value) {
+		
+	}
+	
+	public String deleteMax() {
+		return null;
+	}
+}
