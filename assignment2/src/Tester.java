9a30ec646aa1a07cdcb77cc18652cfef804eb021
diamondlyj <Roger20001107@gmail.com>
2014-11-03 19:43:30 -0500

    update the tester

--- assignment2/src/Tester.java
+++ assignment2/src/Tester.java
@@ -1,9 +1,22 @@
+import java.util.Random;
 
 public class Tester {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	  static final int SIZE=1000;
+          static final int MAX_NUMBER=100000;
+   
+    public static void main(String[]args){
+         My5Heap test=new My5Heap(SIZE);
+         Random x=new Random();
+        double randomNumber=x.nextInt(MAX_NUMBER);
+        for(int i=0;i<1000;i++){
+            test.insert(randomNumber,Double.toString(randomNumber));     //insert 1000 random number into heap   
+        }
+        for(int q=0;q<1000;q++){
+           System.out.println(test.deleteMax()+"No"+q+"Element");
+       }
+    }
 	}
 
 }
