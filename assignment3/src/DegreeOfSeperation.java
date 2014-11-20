403562891503401b96c91f6b2bc733a6da57455c
shunghsiyu <syu07@nyit.edu>
2014-11-20 16:23:05 -0500

    Implement constructor, getCostFromStart(), setCostFromStartIfSmaller in Vertex

--- assignment3/src/DegreeOfSeperation.java
+++ assignment3/src/DegreeOfSeperation.java
@@ -10,14 +10,36 @@ public class DegreeOfSeperation {
 }
 
 class Vertex {
+	public static String INPUT_FILENAME = "facebook_combined.txt";
+	private int id;
+	private boolean initialized;
+	private boolean visited;
+	private double costFromStart;
+	private Entry<Vertex, Integer>[] neighbors;
 	
-	public boolean isAdjacent() {
-		// TODO Implement
-		return false;
+	public Vertex(int id) {
+		this.id = id;
+		this.initialized = false;
+		this.visited = false;
+		this.costFromStart = Double.POSITIVE_INFINITY;
+	}
+	
+	public double getCostFromStart() {
+		return costFromStart;
+	}
+	
+	public boolean setCostFromStartIfSmaller(double newCost) {
+		boolean setSuccess = false;
+		if(newCost < costFromStart) {
+			costFromStart = newCost;
+		}
+		return setSuccess;
 	}
 	
 	public Entry<Vertex, Integer>[] getNeighbors() {
 		// TODO Implement
 		return null;
 	}
+	
+	
 }
